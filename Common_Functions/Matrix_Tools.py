92a742d75b0b179fbbb8d373544c0a615f6c5187
adolescent <39371189+adolescent@users.noreply.github.com>
2025-07-31 19:34:02 +0800

    Work 0731 Done

--- Common_Functions/Matrix_Tools.py
+++ Common_Functions/Matrix_Tools.py
@@ -2,7 +2,7 @@
 These functions will operate matrix, getting the result in need.
 '''
 import numpy as np
-
+from sklearn.decomposition import PCA
 
 
 def Matrix_Clipper(matrix,percent = 99.5):# how much you want to remain unchanged.
@@ -13,3 +13,83 @@ def Matrix_Clipper(matrix,percent = 99.5):# how much you want to remain unchange
     clipped_matrix = np.clip(matrix, lower_bound, upper_bound)
 
     return clipped_matrix
+
+
+def Get_Extreme(matrix,method = 'max',axis = 1,prop=0.05):
+    '''
+    Calculate a matrix's max/min value toward an axis,
+    WORKS ONLY ON 2D MATRIX!!
+
+    '''
+    if axis == 0:# transfer 
+        matrix = matrix.T
+
+    n_cols = matrix.shape[1]
+    k = max(1, int(n_cols * prop))  
+    if method == 'max':
+        partitioned = np.partition(matrix, -k, axis=1)
+        values = partitioned[:, -k:]
+    elif method == 'min':
+        partitioned = np.partition(matrix, k-1, axis=1)
+        values = partitioned[:, :k]
+    else:
+        raise ValueError('Invalid Extreme Method.')
+
+    return values.mean(axis=1)
+
+
+def Corr_Matrix(data,fill_diag=True):
+    """
+    计算样本间的皮尔逊相关系数矩阵并置对角线为0
+    
+    参数:
+    data -- 形状为(d, n)的NumPy数组，d为特征维度，n为样本数量
+            本例中d=250, n=1416
+    
+    返回:
+    corr_matrix -- 形状为(n, n)的皮尔逊相关矩阵，对角线为0
+    """
+    # 转置数据使每行为一个样本 (n x d)
+    samples = data.T
+    
+    # 计算均值
+    mean = np.mean(samples, axis=1, keepdims=True)
+    
+    # 中心化数据
+    centered = samples - mean
+    
+    # 计算标准差 (添加小值避免除零错误)
+    std_dev = np.sqrt(np.sum(centered**2, axis=1))
+    std_dev[std_dev == 0] = 1e-10
+    
+    # 计算协方差矩阵
+    cov_matrix = np.dot(centered, centered.T)
+    
+    # 计算皮尔逊相关系数矩阵
+    corr_matrix = cov_matrix / (std_dev[:, np.newaxis] * std_dev)
+    
+    # 将对角线置为0
+    if fill_diag:
+
+        np.fill_diagonal(corr_matrix, 0)
+    
+    return corr_matrix
+
+
+def Do_PCA(Z_frame,feature = 'Cell',pcnum = 20,method = 'full'):
+    # NOTE: INPUT FRAME SHALL BE IN SHAPE N_Cell*N_Img
+    # feature can be 'Cell' or 'Img', indicating which axis is feature and which is sample.
+    pca = PCA(n_components = pcnum,svd_solver=method) # random
+    data = np.array(Z_frame)
+    if feature == 'Cell':
+        data = data.T# Use cell as sample and frame as feature.
+    elif feature == 'Img':
+        data = data
+    else:
+        raise ValueError('Sample method invalid.')
+    pca.fit(data)
+    PC_Comps = pca.components_# out n_comp*n_feature
+    point_coords = pca.transform(data)# in n_sample*n_feature,out n_sample*n_comp
+    return PC_Comps,point_coords,pca
+
+
--- _Projects/250725_GoodUnit_Transform/Test_Trans.py
+++ _Projects/250725_GoodUnit_Transform/Test_Trans.py
@@ -15,6 +15,8 @@ from scipy import stats
 import OS_Tools as ot
 import pandas as pd
 from Spike_Tools import *
+from Matrix_Tools import *
+import cv2
 
 gn_path = r'D:\#Data\silct\GoodUnit_250411_JianJian_silct_npx_1416_g0_MSB.mat'
 savepath= r'D:\#Data\silct\Trans_Response'
@@ -111,17 +113,171 @@ reordered_response = tuned_response[:, sorted_index]
 fig,ax = plt.subplots(ncols=1,nrows=1,sharex=True,dpi = 300,figsize = (7,3))
 sns.heatmap(reordered_response,center=0,vmax = v_bound,vmin = -v_bound,ax =ax,cbar=False)
 
-#%% scatter each cell's response, getting it's 
+#%% scatter each cell's response, getting it's best response toward a category.
+text_id = np.array(stim_info[stim_info['FOB'].isin(['Texture'])].index)
+boulder_id = np.array(stim_info[stim_info['FOB'].isin(['Boulder'])].index)
+filled_id = np.array(stim_info[stim_info['FOB'].isin(['Filled'])].index)
+fob_id = np.array(stim_info[stim_info['FOB'].isin(['Face','Body','Object'])].index)
+text_resp = tuned_response[:,text_id]
+boulder_resp = tuned_response[:,boulder_id]
+filled_resp = tuned_response[:,filled_id]
+fob_resp = tuned_response[:,fob_id]
+prop = 0.2
+text_best = Get_Extreme(text_resp,'max',1,prop)
+boulder_best = Get_Extreme(boulder_resp,'max',1,prop)
+filled_best = Get_Extreme(filled_resp,'max',1,prop)
+fob_best = Get_Extreme(fob_resp,'max',1,prop)
+# getting a scattered response, and plot 
+best_resp_frame = pd.DataFrame(0.0,index=range(len(text_best)),columns = ['Texture','Filled','Boulder','FOB'])
+for i in range(len(text_resp)):
+    best_resp_frame.iloc[i,:] = [text_best[i],filled_best[i],boulder_best[i],fob_best[i]]
+
+fig,ax = plt.subplots(nrows=2,ncols=2,dpi=240,figsize = (7,7))
+for i in range(2):
+    for j in range(2):
+        ax[i][j].plot([0,5],[0,5],linestyle='--',color='gray',alpha=0.7)
+        ax[i][j].set_ylim(0,5)
+        ax[i][j].set_xlim(0,5)
+
+sns.scatterplot(data = best_resp_frame,x='FOB',y='Filled',ax = ax[0][0],lw=0,s=3)
+sns.scatterplot(data = best_resp_frame,x='Texture',y='Filled',ax = ax[0][1],lw=0,s=3)
+sns.scatterplot(data = best_resp_frame,x='Boulder',y='Texture',ax = ax[1][0],lw=0,s=3)
+sns.scatterplot(data = best_resp_frame,x='Boulder',y='Filled',ax = ax[1][1],lw=0,s=3)
+
+fig.tight_layout()
+
+#%%######################### Another version, figure-based analysis
+'''
+Here we try to analyze graph's respone on 3 different situations.
+
+'''
+figure_response_frame = pd.DataFrame(index=range(text_resp.shape[1]),columns = ['Boulder_vec','Texture_vec','Filled_vec','Boulder_Resp','Filled_Resp','Texture_Resp','Corr_FB','Corr_FT','Corr_BT'])
+
+# get response vector of each figure, and calculate similarity.
+strength_prop = 0.5
+k = int(len(tuned_response)*strength_prop)
+for i in range(text_resp.shape[1]):
+    c_text_vec = tuned_response[:,i*3]
+    c_boulder_vec = tuned_response[:,i*3+1]
+    c_filled_vec = tuned_response[:,i*3+2]
+    c_text_resp = np.partition(c_text_vec, -k)[-k:].mean()
+    c_boulder_resp = np.partition(c_boulder_vec, -k)[-k:].mean()
+    c_filled_resp = np.partition(c_filled_vec, -k)[-k:].mean()
+    # calculate pearsonr 
+    c_bt,_ = stats.pearsonr(c_boulder_vec,c_text_vec)
+    c_fb,_ = stats.pearsonr(c_filled_vec,c_boulder_vec)
+    c_ft,_ = stats.pearsonr(c_filled_vec,c_text_vec)
+    figure_response_frame.loc[i,:] = [c_boulder_vec,c_text_vec,c_filled_vec,c_boulder_resp,c_filled_resp,c_text_resp,c_fb,c_ft,c_bt]
+    
+figure_response_frame['Boulder_Resp'] = figure_response_frame['Boulder_Resp'].astype('f8')
+figure_response_frame['Filled_Resp'] = figure_response_frame['Filled_Resp'].astype('f8')
+figure_response_frame['Texture_Resp'] = figure_response_frame['Texture_Resp'].astype('f8')
+figure_response_frame['Corr_FB'] = figure_response_frame['Corr_FB'].astype('f8')
+figure_response_frame['Corr_FT'] = figure_response_frame['Corr_FT'].astype('f8')
+figure_response_frame['Corr_BT'] = figure_response_frame['Corr_BT'].astype('f8')
 
 
 
+fig,ax = plt.subplots(nrows=2,ncols=2,dpi=240,figsize = (7,7))
+sns.scatterplot(data = figure_response_frame,x='Boulder_Resp',y='Filled_Resp',lw=0,s=3,ax=ax[0][0])
+sns.scatterplot(data = figure_response_frame,x='Texture_Resp',y='Filled_Resp',lw=0,s=3,ax=ax[0][1])
+sns.scatterplot(data = figure_response_frame,x='Boulder_Resp',y='Texture_Resp',lw=0,s=3,ax=ax[1][0])
+for i in range(2):
+    for j in range(2):
+        if not (i==1)*(j==1): 
+            ax[i][j].plot([0,3],[0,3],linestyle='--',color='gray',alpha=0.7)
+            ax[i][j].set_ylim(0,3)
+            ax[i][j].set_xlim(0,3)
+ax[1][1].hist(figure_response_frame['Boulder_Resp']-figure_response_frame['Texture_Resp'],bins=25)
+ax[1][1].axvline(0,linestyle = '--',alpha=0.5,color = 'gray')
+ax[1][1].set_xlabel('Boulder-Texture')
+fig.tight_layout()
+#%% plot 3 condition response similarity.
+figure_response_frame['Graph_ID'] = np.array(figure_response_frame.index)+1
+melted_frame = figure_response_frame.melt(id_vars='Graph_ID',value_vars = ['Corr_FB','Corr_FT','Corr_BT'],value_name='Correlation',var_name='Pair')
+sns.histplot(data = melted_frame,x='Correlation',hue = 'Pair',bins = 30,lw=0,common_norm=True,common_bins=True,stat='density',alpha=0.5)
+
+#%% Demonstration of boulder best and texture best
+figpath = r'D:\#Data\#stimuli\silct\silct_npx_1416'
+figure_response_frame['Boulder-Texture'] = figure_response_frame['Boulder_Resp']-figure_response_frame['Texture_Resp']
+a = figure_response_frame.sort_values(by=['Boulder-Texture'],ascending=False)
+boulder_figs = np.array(a.iloc[:18].index)
+texture_figs = np.array(a.iloc[-18:].index)
+
+# plot boulder figures.
+fig,ax = plt.subplots(ncols=3,nrows=6,figsize = (12,12),dpi=180)
+for i in range(18):
+    c_graph_id = texture_figs[i] # x3+1 is the first texture graph.
+    c_texture = str(10000+c_graph_id*3+1)[1:]+'.jpg'
+    c_boulder = str(10000+c_graph_id*3+2)[1:]+'.jpg'
+    c_texture_graph = cv2.imread(ot.Join(figpath,c_texture),0)
+    c_boulder_graph = cv2.imread(ot.Join(figpath,c_boulder),0)
+    c_compare = np.concatenate([c_texture_graph,c_boulder_graph],axis=1)
+    ax[i//3,i%3].imshow(c_compare,cmap='gray')
+    ax[i//3,i%3].set_yticks([])
+    ax[i//3,i%3].set_xticks([])
+fig.tight_layout()
+
+#%% Demonstration of Filled-Texture-Boulder most similar 
+a = figure_response_frame.sort_values(by=['Corr_FB'],ascending=False)
+best_FB_sim = np.array(a.iloc[:18].index)
+worst_FB_sim = np.array(a.iloc[-18:].index)
+
+fig,ax = plt.subplots(ncols=3,nrows=6,figsize = (12,12),dpi=180)
+for i in range(18):
+    c_graph_id = worst_FB_sim[i] # x3+1 is the first texture graph.
+    c_texture = str(10000+c_graph_id*3+2)[1:]+'.jpg'
+    c_boulder = str(10000+c_graph_id*3+3)[1:]+'.jpg'
+    c_texture_graph = cv2.imread(ot.Join(figpath,c_texture),0)
+    c_boulder_graph = cv2.imread(ot.Join(figpath,c_boulder),0)
+    c_compare = np.concatenate([c_texture_graph,c_boulder_graph],axis=1)
+    ax[i//3,i%3].imshow(c_compare,cmap='gray')
+    ax[i//3,i%3].set_yticks([])
+    ax[i//3,i%3].set_xticks([])
+fig.tight_layout()
+
+#%%############################# RSA analysis, getting similarity between graphs.
+# re-arrange graphs, in order 
+
+vectors = np.concatenate([text_resp,boulder_resp,filled_resp],axis=1)
+sim_matrix = Corr_Matrix(vectors)
 
 
+fig,ax = plt.subplots(nrows=1,ncols=1,dpi=330,figsize=(6,6))
+sns.heatmap(sim_matrix,center=0,ax = ax,square=True,vmax = 0.5,vmin = -0.35,cbar=False)
+ax.plot([0,1200],[400,400],color='y',lw=1,alpha=0.5)
+ax.plot([0,1200],[800,800],color='y',lw=1,alpha=0.5)
+ax.plot([400,400],[0,1200],color='y',lw=1,alpha=0.5)
+ax.plot([800,800],[0,1200],color='y',lw=1,alpha=0.5)
+
+ax.set_xticks([0,200,400,600,800,1000,1200])
+ax.set_xticklabels([0,200,400,600,800,1000,1200])
+ax.set_yticks([0,200,400,600,800,1000,1200])
+ax.set_yticklabels([0,200,400,600,800,1000,1200])
+#%% find universal correlated stim between filled and texture-boulder.
+# tex_fill_matrix = sim_matrix[800:,:400]
+boulder_fill_matrix = sim_matrix[800:,400:800]
+thres = 0.35
+# corr_counts = pd.DataFrame((tex_fill_matrix>0.35).sum(0))
+corr_counts = pd.DataFrame((boulder_fill_matrix>0.35).sum(0))
+tex_index = corr_counts.sort_values(by=[0],ascending=False)
+best_tex = np.array(tex_index.iloc[:36].index)
+
+fig,ax = plt.subplots(ncols=6,nrows=6,figsize = (12,12),dpi=180)
+for i in range(36):
+    c_graph_id = best_tex[i] # x3+1 is the first texture graph.
+    c_texture = str(10000+c_graph_id*3+2)[1:]+'.jpg'
+    c_texture_graph = cv2.imread(ot.Join(figpath,c_texture),0)
+    ax[i//6,i%6].imshow(c_texture_graph,cmap='gray')
+    ax[i//6,i%6].set_yticks([])
+    ax[i//6,i%6].set_xticks([])
+
+fig.tight_layout()
 
 
-
-
-
-
+#%% ############################## PCA Analysis ##################
+'''
+Use all neurons as 
+'''
 
 
