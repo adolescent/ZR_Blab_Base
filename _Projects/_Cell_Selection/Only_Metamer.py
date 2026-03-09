38a6702f109f6063aa0d54e5129fcd12b9058694
adolescent <39371189+adolescent@users.noreply.github.com>
2026-03-09 16:58:44 +0800

    Metamer method adjusted
    through cursor.

--- _Projects/_Cell_Selection/Only_Metamer.py
+++ _Projects/_Cell_Selection/Only_Metamer.py
@@ -18,70 +18,130 @@ from Common_Functions.Useful_Plotter import *
 import copy
 import warnings
 import gc
+import pandas as pd
+import numpy as np
 warnings.filterwarnings("ignore")
 
 msb_sites = ot.Get_File_Name(r'E:\#Preprocessed_Data\SiteClass\Metamers\MSB','.joblib')
 save_path = r'E:\#Preprocessed_Data\Selected_Cells'
+target_area = 'MSB'
 #%%
-cell_counter = 0
-for i,cloc in tqdm(enumerate(msb_sites)):
-    print(cloc.split('\\')[-1])
+def _get_stimset_series(stim_info):
+    """
+    Return a pandas Series of Stim_Set labels if available; otherwise None.
+    Handles both DataFrame and objects with attribute access.
+    """
+    if stim_info is None:
+        return None
+    if isinstance(stim_info, pd.DataFrame) and ('Stim_Set' in stim_info.columns):
+        return stim_info['Stim_Set']
+    if hasattr(stim_info, 'Stim_Set'):
+        return pd.Series(getattr(stim_info, 'Stim_Set'))
+    return None
+
+
+all_metamer_resp_list = []
+all_fob_resp_list = []
+all_d_primes_list = []
+all_response_list = []
+expected_fob_n = 300
+
+for _, cloc in tqdm(enumerate(msb_sites), total=len(msb_sites)):
+    fname = cloc.split('\\')[-1]
+    print(fname)
     a = JL.load(cloc)
     c_info = a.Site_Info
     c_loc = a.site_name
-    if ('ML' not in a.brain_areas) and ('MF' not in a.brain_areas):
-    # if 'AL' not in a.brain_areas:
-        print(f'Not Correct Area, ignore {cloc.split('\\')[-1]}.')
+
+    # select brain area
+    # if ('ML' not in a.brain_areas) and ('MF' not in a.brain_areas):
+    if target_area not in a.brain_areas:
+        print(f'Not Correct Area, ignore {fname}.')
         del a
         gc.collect()
         continue
-    ok_cells = c_info[c_info.Ceiling_Index>(0.3/1.7)]
-    # body_cells = ok_cells[ok_cells.Best_Prefer=='Body']
-    body_cells = ok_cells[ok_cells.Best_Prefer=='Face']
-    # body_cells = np.array(ok_cells[ok_cells.Best_D_Prime>0.5].Cell)
-    body_cells = np.array(body_cells[body_cells.Best_D_Prime>0.5].Cell)
-    body_resps = a.avr_psth[body_cells,:,:]
-    raw_rsp = a.avr_psth
-    if a.stimset == 'Metamer1072':
-        metamers = body_resps[:,72:,:]
-    elif a.stimset == 'Metamer1300':
-        metamers = body_resps[:,:1000,:]
-    else:
-        metamers = body_resps[:,300:1300,:]
-    # generate concated response.
-    try:# If no var, init.
-        all_matemer_resp = np.concatenate([all_matemer_resp,metamers],axis=0)
-    except:
-        all_matemer_resp = copy.deepcopy(metamers)
 
-    # generate cell infos.
+    ok_cells = c_info[c_info.Ceiling_Index > (0.3 / 1.7)]
+    if target_area == 'MSB' or target_area == 'ASB':
+        selected_cells = ok_cells[ok_cells.Best_Prefer == 'Body']
+    else:
+        selected_cells = ok_cells[ok_cells.Best_Prefer == 'Face']
+    selected_cells = np.array(selected_cells[selected_cells.Best_D_Prime > 0.5].Cell)
+    if selected_cells.size == 0:
+        del a
+        gc.collect()
+        continue
+
+    selected_resps = a.avr_psth[selected_cells, :, :]
+
+    # Metamer subset (kept as your original logic)
+    if a.stimset == 'Metamer1072':
+        metamers = selected_resps[:, 72:, :]
+    elif a.stimset == 'Metamer1300':
+        metamers = selected_resps[:, :1000, :]
+    else:
+        metamers = selected_resps[:, 300:1300, :]
+    all_metamer_resp_list.append(metamers)
+
+    # FOB subset: indices where stim_info.Stim_Set contains 'FOB'
+    stim_set = _get_stimset_series(getattr(a, 'stim_info', None))
+    if stim_set is None:
+        fob_resp = selected_resps[:, :0, :]  # empty but concat-safe if consistent
+    else:
+        fob_sets = np.where(stim_set.astype(str).str.contains('FOB', na=False).to_numpy())[0]
+        fob_resp = selected_resps[:, fob_sets, :]
+
+    if expected_fob_n is None and fob_resp.shape[1] > 0:
+        expected_fob_n = fob_resp.shape[1]
+    elif expected_fob_n is not None and fob_resp.shape[1] != expected_fob_n:
+        # Keep concatenation robust across sites with slightly different FOB counts
+        print('Warning of FOB: Not STI 150*2. Fill with 300.')
+        min_n = min(expected_fob_n, fob_resp.shape[1])
+        fob_resp = fob_resp[:, :min_n, :]
+        # expected_fob_n = min_n
+    all_fob_resp_list.append(fob_resp)
+
+    # cell info tables
     c_dps = a.Cell_FOB_DPrimes
     c_rsp = a.Cell_FOB_Response_avr
-    c_dps = c_dps[c_dps['Cell'].isin(body_cells)] # only body-selected part
-    c_rsp = c_rsp[c_rsp['Cell'].isin(body_cells)]
+    c_dps = c_dps[c_dps['Cell'].isin(selected_cells)].copy()
+    c_rsp = c_rsp[c_rsp['Cell'].isin(selected_cells)].copy()
 
-    # add loc.
+    # add loc and stimset (same as Doodle_Acquire.py)
     c_dps['Loc'] = c_loc
     c_rsp['Loc'] = c_loc
+    c_dps['Stimset'] = a.stimset
+    c_rsp['Stimset'] = a.stimset
 
-    try:
-        all_response = pd.concat((all_response,c_rsp))
-    except:
-        all_response = copy.deepcopy(c_rsp)
-    try:
-        all_d_primes = pd.concat((all_d_primes,c_dps))
-    except:
-        all_d_primes = copy.deepcopy(c_dps)
+    all_d_primes_list.append(c_dps)
+    all_response_list.append(c_rsp)
 
     del a
     gc.collect()
 
-all_d_primes['Cell_ID'] = all_d_primes.groupby(['Loc', 'Cell'], sort=False).ngroup()
-all_response['Cell_ID'] = all_response.groupby(['Loc', 'Cell'], sort=False).ngroup()
+all_matemer_resp = np.concatenate(all_metamer_resp_list, axis=0) if len(all_metamer_resp_list) else np.empty((0, 0, 0))
+all_fob_resp = np.concatenate(all_fob_resp_list, axis=0) if len(all_fob_resp_list) else np.empty((0, 0, 0))
+all_d_primes = pd.concat(all_d_primes_list, ignore_index=True) if len(all_d_primes_list) else pd.DataFrame()
+all_response = pd.concat(all_response_list, ignore_index=True) if len(all_response_list) else pd.DataFrame()
+
+if len(all_d_primes):
+    all_d_primes['Cell_ID'] = all_d_primes.groupby(['Loc', 'Cell'], sort=False).ngroup()
+if len(all_response):
+    all_response['Cell_ID'] = all_response.groupby(['Loc', 'Cell'], sort=False).ngroup()
+#%% ## Plot response heatmap.
+fob_avr = all_fob_resp[:,:,160:320].sum(-1)
+plotable = fob_avr/fob_avr.max(1,keepdims = True)
+sns.heatmap(plotable,center=0,cmap='bwr')
 
 
 #%% save all msb cells
-np.savez_compressed(ot.Join(save_path,'ML_Cells_Metamer_Only.npz'),psth = all_matemer_resp,d_primes = all_d_primes,response = all_response)
+np.savez_compressed(
+    ot.Join(save_path, f'{target_area}_Cells_Metamer_Only.npz'),
+    psth=all_matemer_resp,
+    FOB=all_fob_resp,
+    d_primes=all_d_primes,
+    response=all_response
+)
 
 print(set(all_d_primes.Loc))
 #%% counts 
