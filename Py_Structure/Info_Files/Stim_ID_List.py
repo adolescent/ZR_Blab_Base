beea0c2203bdc85c56e07442df93e09ebe9aa6f1
adolescent <39371189+adolescent@users.noreply.github.com>
2026-04-21 13:36:45 +0800

    Anagram Jigsaw Info added

--- Py_Structure/Info_Files/Stim_ID_List.py
+++ Py_Structure/Info_Files/Stim_ID_List.py
@@ -11,9 +11,12 @@ class Stim_ID(object):
 
     def __init__(self,stim_type='Metamer_Raw'):
 
+        print(f'Loading {stim_type} info...')
+
         if stim_type == 'Metamer_Raw':
             self.Metamer_Info_Raw()
-        
+        elif stim_type == 'Anagram_Jigsaw':
+            self.Anagram_Jigsaw_Info_Raw()
         # return self.Stim_Conditions
 
     def Metamer_Info_Raw(self):
@@ -28,3 +31,37 @@ class Stim_ID(object):
             'Shuffle_Level': shuffle_levels
         })
         # return self.Stim_Conditions
+
+    def Anagram_Jigsaw_Info_Raw(self):
+        total_repeats = 3
+        styles = np.arange(1, 6)  # 5 styles
+        pairs_per_style = 20
+        ab_labels = ['A', 'B']
+        pair_type_labels = ['FF', 'FA', 'AI', 'II']
+
+        records = []
+
+        # Each repeat has 200 images = 100 pairs = 5 styles * 20 pairs/style
+        for _ in range(total_repeats):
+            for pair_id in range(1, pairs_per_style + 1):
+                # Pair_Tyle is tied to object pair (Img_Index), not style.
+                # Each Img_Index occupies 10 rows (5 styles * A/B), all with same Pair_Tyle.
+                pair_type = pair_type_labels[(pair_id - 1) % 4]
+                for style in styles:
+                    for ab in ab_labels:
+                        records.append({
+                            'Img_Index': pair_id,
+                            'Style': style,
+                            'Pair_Tyle': pair_type,
+                            'AB': ab
+                        })
+
+        self.Stim_Conditions = pd.DataFrame(records)
+
+        assert len(self.Stim_Conditions) == 600
+
+#%%
+if __name__ == '__main__':
+    Stim_ID = Stim_ID('Anagram_Jigsaw')
+    stim_info = Stim_ID.Stim_Conditions
+    stim_info.iloc[:40]
